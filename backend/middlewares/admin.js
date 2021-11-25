3d35aafa6c8df51b869210743e6d0624870d4d16
DiomedesDiaz <juanrojas0399@gmail.com>
2021-11-25 09:19:15 -0500

    Middlewares

--- backend/middlewares/admin.js
+++ backend/middlewares/admin.js
@@ -2,11 +2,11 @@ import role from "../models/role.js";
 
 const admin = async (req, res, next) => {
   const adminRole = await role.findById(req.user.roleId);
-  if (!adminRole) return res.status(400).send({ message: "Role no found" });
+  if (!adminRole) return res.status(400).send({ message: "Rol no encontrado" });
 
   return adminRole.name === "admin"
     ? next()
-    : res.status(400).send({ message: "Unauthorized user" });
+    : res.status(400).send({ message: "Usuario no autorizado" });
 };
 
 export default admin;
